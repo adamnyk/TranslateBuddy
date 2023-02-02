425d277fb8b31fc6a3d670f92aec277243a009e3
Adam Pecan <adamnyk@gmail.com>
2023-02-02 09:58:22 -0700

    added translate form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 """Forms for Travel Translator"""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, BooleanField
+from wtforms import StringField, PasswordField, SelectField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class UserAddForm(FlaskForm):
@@ -16,3 +16,11 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+    
+class TranslateForm(FlaskForm):
+    """Text translation form."""
+    
+    translate_text = TextAreaField('Text to translate', validators=[Length(max=100), DataRequired()])
+    target_lang = SelectField('Translate into:', validators=[DataRequired()])
+    
+    
